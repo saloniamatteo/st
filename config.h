936446854b10be5a329a753cfc1d6284757db768
saloniamatteo <saloniamatteo@pm.me>
2021-03-03 08:57:35 +0100

    Update font settings

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=14:antialias=true:autohint=true:hintfull=true";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
