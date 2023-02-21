750c55cff4e79d40a6b47eb91b66cd9fe055a333
saloniamatteo <saloniamatteo@pm.me>
2023-02-21 10:24:14 +0100

    Use Iosevka font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FuraCode Nerd Font:pixelsize=15:antialias=true:autohint=true:hintfull=true";
+static char *font = "Iosevka Nerd Font Mono:pixelsize=15:antialias=true:autohint=true:hintfull=true";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
